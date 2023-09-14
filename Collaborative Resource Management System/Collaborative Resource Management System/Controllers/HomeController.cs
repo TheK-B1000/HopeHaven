5cb51df960b6c31cbacbaedc0086dd2d9a05de3e
NighttimeAura74 <103013348+NighttimeAura74@users.noreply.github.com>
2023-09-13 22:42:03 -0400

    Initial Creation
    Set up Views and controllers
    Set up a few pages for CRMS Demo

---
+++ Collaborative Resource Management System/Collaborative Resource Management System/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Collaborative_Resource_Management_System.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Collaborative Resource Management System/Collaborative Resource Management System/Program.cs
+++ Collaborative Resource Management System/Collaborative Resource Management System/Program.cs
@@ -1,6 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseAuthorization();
+app.MapGet("/hi", () => "Hello!");
+app.MapDefaultControllerRoute();
+app.MapRazorPages();
 app.Run();
