8e15bc6595b2247aa81b5458cfa82eb0718c3dab
NighttimeAura74 <103013348+NighttimeAura74@users.noreply.github.com>
2023-09-13 23:20:28 -0400

    Added views
    Added views for each roles links

---
+++ Collaborative Resource Management System/Collaborative Resource Management System/Controllers/InventoryController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Collaborative_Resource_Management_System.Controllers
+{
+    public class InventoryController : Controller
+    {
+        public IActionResult CheckIn()
+        {
+            return View();
+        }
+        public IActionResult CheckOut()
+        {
+            return View();
+        }
+        public IActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
