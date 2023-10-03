13aa26fc6ac4aceac11c8e97feda52b7650eafab
Kevin-Brandon Joseph Corbett <k-b0514@hotmail.com>
2023-10-03 17:14:37 -0400

    Add AppDbContext

--- Collaborative Resource Management System/Collaborative Resource Management System/Models/Department.cs
+++ Collaborative Resource Management System/Collaborative Resource Management System/Models/Department.cs
@@ -4,8 +4,8 @@
     {
         public int DeptID { get; set; }
         public string DeptName { get; set; }
-        public int CreatedBy { get; set; }
-        public int EditedBy { get; set; }
+        public string CreatedBy { get; set; }
+        public string EditedBy { get; set; }
         public DateTime CreatedDate { get; set; }
         public DateTime EditedDate { get; set; }
         public bool Active { get; set; }
--- Collaborative Resource Management System/Collaborative Resource Management System/Program.cs
+++ Collaborative Resource Management System/Collaborative Resource Management System/Program.cs
@@ -1,7 +1,15 @@
+using Collaborative_Resource_Management_System.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
