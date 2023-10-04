acf072fc39170cb6638a28e2f76c79f7b9c4f92f
Kevin-Brandon Joseph Corbett <k-b0514@hotmail.com>
2023-10-04 15:38:37 -0400

    Update NonConsumable

--- Collaborative Resource Management System/Collaborative Resource Management System/Models/NonConsumable.cs
+++ Collaborative Resource Management System/Collaborative Resource Management System/Models/NonConsumable.cs
@@ -4,6 +4,7 @@ namespace Collaborative_Resource_Management_System.Models
 {
     public class NonConsumable
     {
+        public int NonConsumableID { get; set; }
         public string AssetTag { get; set; }
         public int ItemID { get; set; }
 
