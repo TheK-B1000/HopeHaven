55daaa045c3747a65329448c09bc21d178fa3c01
Kevin-Brandon Joseph Corbett <k-b0514@hotmail.com>
2023-10-05 18:54:23 -0400

    Add DbInitializer

---
+++ Collaborative Resource Management System/Collaborative Resource Management System/Models/DbInitializer.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+
+namespace Collaborative_Resource_Management_System.Models
+{
+    public static class DbInitializer
+    {
+        public static void SeedData(IServiceProvider serviceProvider)
+        {
+            using (var scope = serviceProvider.CreateScope())
+            {
+                var scopedServiceProvider = scope.ServiceProvider;
+                var context = scopedServiceProvider.GetRequiredService<AppDbContext>();
+
+               
+            }
+        }
+    }
+}
