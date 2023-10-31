6c0ca60765337b45c14936ad2b69c70f96653ac6
Kevin-Brandon Joseph Corbett <k-b0514@hotmail.com>
2023-10-31 16:14:37 -0400

    Add Transaction Model

--- Collaborative Resource Management System/Collaborative Resource Management System/Models/AppDbContext.cs
+++ Collaborative Resource Management System/Collaborative Resource Management System/Models/AppDbContext.cs
@@ -17,6 +17,6 @@ namespace Collaborative_Resource_Management_System.Models
         public DbSet<Category> Categories { get; set; }
         public DbSet<CheckIn> CheckIns { get; set; }
         public DbSet<InventoryIntake> InventoryIntakes { get; set; }
-
+        public DbSet<Transaction> Transactions { get; set; }
     }
 }
---
+++ Collaborative Resource Management System/Collaborative Resource Management System/Models/Transaction.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Collaborative_Resource_Management_System.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionID { get; set; }
+        public int DepartmentID { get; set; }
+        public int UserID { get; set; }
+    }
+}
