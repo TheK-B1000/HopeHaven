45fbddf4f5b9778ecfa65c2627c1ef9b4e6258be
Kevin-Brandon Joseph Corbett <k-b0514@hotmail.com>
2023-11-17 11:01:31 -0500

    Update Report to show name of user, item, and department

--- Collaborative Resource Management System/Collaborative Resource Management System/Controllers/ReportController.cs
+++ Collaborative Resource Management System/Collaborative Resource Management System/Controllers/ReportController.cs
@@ -1,5 +1,6 @@
 ﻿using Collaborative_Resource_Management_System.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Collaborative_Resource_Management_System.Controllers
 {
@@ -14,9 +15,22 @@ namespace Collaborative_Resource_Management_System.Controllers
 
         public IActionResult Report()
         {
-            var checkouts = _dbContext.CheckOuts.ToList();
+            var checkouts = _dbContext.CheckOuts
+                            .Include(co => co.User)
+                            .Include(co => co.Item)
+                            .Include(co => co.Department)
+                            .Select(co => new CheckOutViewModel
+                            {
+                                User = co.User.Name, 
+                                Item = co.Item.Name, 
+                                CheckOutDate = co.CheckOutDate,
+                                Price = co.Price,
+                                Department = co.Department.DeptName 
+                            })
+                            .ToList();
             return View(checkouts);
         }
+
         public IActionResult Confirmation()
         {
             return View();
---
+++ Collaborative Resource Management System/Collaborative Resource Management System/Models/CheckOutViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Collaborative_Resource_Management_System.Models
+{
+    public class CheckOutViewModel
+    {
+        public string User { get; set; }
+        public string Item { get; set; }
+        public DateTime CheckOutDate { get; set; }
+        public float Price { get; set; }
+        public string Department { get; set; }
+    }
+}
